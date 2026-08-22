0f5c3e02b9610c8a2622437b1d97ff1d10c258f8
Simon Cropp <simon.cropp@gmail.com>
2026-08-22 17:38:03 +1000

    Update GlobalUsings.cs

--- src/Krilla.Html/GlobalUsings.cs
+++ src/Krilla.Html/GlobalUsings.cs
@@ -11,4 +11,5 @@ global using Krilla.Html;
 global using Krilla.Html.Fonts;
 global using Krilla.Html.Images;
 global using Krilla.Html.Layout;
+global using Krilla.Html.Painting;
 global using Krilla.Html.Styling;
